be7bdacfe9815d17cffcd9a5f052257b9d6099a3
Benjamin T. Johnson <benjamin.t.johnson@noaa.gov>
2023-10-19 01:32:04 +0000

    updated internal versioning to be v3.1.0 in preparation for release.

--- Get_CRTM_Binary_Files.sh
+++ Get_CRTM_Binary_Files.sh
@@ -1,29 +1,31 @@
-#https://bin.ssec.wisc.edu/pub/s4/CRTM/fix_REL-3.0.0_20230802.tgz
-foldername="fix_REL-3.0.0_20230802"
+#https://bin.ssec.wisc.edu/pub/s4/CRTM/file/crtm_coefficients_3.1.0_skylab_7.0.tar.gz
+foldername="3.1.0_skylab_7.0"
+filename="crtm_coefficients_${foldername}.tar.gz"
+echo "$filename"
+break
 
-filename="${foldername}.tgz"
 if test -f "$filename"; then
     if [ -d "fix/" ]; then #fix directory exists
         echo "fix/ already exists, doing nothing."
     else
         #untar the file and move directory to fix
 				tar -zxvf $filename
-				cd $foldername/
-				mv fix ..
-				cd ..
-				rmdir $foldername
+				mkdir fix
+				mv crtm/$foldername/* fix/.
+				rm -rf $foldername
 				echo "fix/ directory created from existing $filename file."
     fi 
 else
     #download, untar, move
 		echo "Downloading $filename, please wait about 5 minutes (4 GB tar file)"
-	  wget  https://bin.ssec.wisc.edu/pub/s4/CRTM/$filename # CRTM binary files, add "-q" to suppress output. 
+	  wget  https://bin.ssec.wisc.edu/pub/s4/CRTM/file/$filename # CRTM binary files, add "-q" to suppress output. 
 		
-	  tar -zxvf $filename
-		cd $foldername/
-		mv fix ..
-		cd ..
-		rmdir $foldername
-		echo "fix/ directory created from downloaded $filename."
+		
+    #untar the file and move directory to fix
+    tar -zxvf $filename
+    mkdir fix
+    mv crtm/$foldername/* fix/.
+    rm -rf crtm/$foldername
+  	echo "fix/ directory created from downloaded $filename."
 fi
 echo "Completed."
