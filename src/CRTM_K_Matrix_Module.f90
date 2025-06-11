cfb38a2121725a7425bd492c767ba32d760ede7a
Benjamin T. Johnson <benjamin.t.johnson@noaa.gov>
2025-06-11 17:56:29 +0000

    restoring inadvertent overwrite of CRTM_K_Matrix_Module.f90 when merging with develop.

--- src/CRTM_K_Matrix_Module.f90
+++ src/CRTM_K_Matrix_Module.f90
@@ -428,14 +428,22 @@ CONTAINS
       CALL OMP_SET_MAX_ACTIVE_LEVELS(1)
     ELSE
       n_profile_threads = n_Profiles
+
+!** BTJ: temporary preprocessor directive for openMP over channels bypass, permitting modern ifort / ifx versions to run properly
+!** https://github.com/JCSDA/CRTMv3/issues/231
+
+#if 1
+      n_channel_threads = 1
+#else
       n_channel_threads = MIN(n_Channels, n_omp_threads / n_Profiles)
-!      n_channel_threads = MIN(n_Channels, n_omp_threads )
-      if(n_channel_threads > 1) THEN
+#endif
+      IF(n_channel_threads > 1) THEN
         CALL OMP_SET_MAX_ACTIVE_LEVELS(2)
       ELSE
         CALL OMP_SET_MAX_ACTIVE_LEVELS(1)
       END IF
     END IF
+
 
 !    WRITE(6,*)
 !    WRITE(6,'("   Using",i3," OpenMP threads =",i3," for profiles and",i3," for channels.")') &
@@ -979,20 +987,15 @@ CONTAINS
         ! ------------
         ! THREAD LOOP
         ! ------------
-
-#ifdef __INTEL_LLVM_COMPILER
-        ! Intel ifx (LLVM) then skip OpenMP
-#elif defined(__INTEL_COMPILER)
-#if __INTEL_COMPILER < 2024
-!$OMP PARALLEL DO NUM_THREADS(n_channel_threads)                        &
-!$OMP    FIRSTPRIVATE(ln, r_cloudy)                                     &
-!$OMP    PRIVATE(Message, ChannelIndex, n_Full_Streams, AAvar,          &
-!$OMP            start_ch, end_ch, Wavenumber, Status_FWD, Status_K,    &
-!$OMP            transmittance, transmittance_K, transmittance_clear,   &
-!$OMP            transmittance_clear_K, l, mth_Azi, ks)
-#else
-        ! skip ifort 2024
-#endif
+!** BTJ preprocessor directive bypass of OMP directives causing issues when compiling with modern ifort/ifx
+!** https://github.com/JCSDA/CRTMv3/issues/231
+#if 1
+        IF (n_channel_threads > 1) THEN
+           WRITE( Message,'("ERROR: n_channel_threads > 1, this should not happen with the current preprocessor directives")')
+           
+           Error_status = FAILURE
+           CALL Display_Message( ROUTINE_NAME, Message, Error_Status )
+        END IF
 #else
 !$OMP PARALLEL DO NUM_THREADS(n_channel_threads)                        &
 !$OMP    FIRSTPRIVATE(ln, r_cloudy)                                     &
@@ -1666,18 +1669,15 @@ CONTAINS
             END IF
 
           END DO Channel_Loop
-        END DO Thread_Loop
-#ifdef __INTEL_LLVM_COMPILER
-        ! skip
-#elif defined(__INTEL_COMPILER)
-   #if __INTEL_COMPILER < 2024
-!$OMP END PARALLEL DO
-   #endif
-#else
-!$OMP END PARALLEL DO
-#endif
-        IF ( Error_Status == FAILURE ) RETURN
+       END DO Thread_Loop
 
+!** BTJ preprocessor directive bypass of OMP directives causing issues when compiling with modern ifort/ifx
+!** https://github.com/JCSDA/CRTMv3/issues/231
+#if 0
+!$OMP END PARALLEL DO
+#endif         
+
+        IF ( Error_Status == FAILURE ) RETURN
         ln = ln + n_sensor_channels - n_inactive_channels(n_channel_threads + 1)
 
       END DO Sensor_Loop
