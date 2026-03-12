e3b989f1b541b741de96c534a95e8139ad00962f
Francois Hebert <hebert@ucar.edu>
2026-03-12 19:18:04 +0000

    Clean up compiler flag specification

--- cmake/crtm_compiler_flags.cmake
+++ cmake/crtm_compiler_flags.cmake
@@ -1,37 +1,27 @@
+# (C) Copyright 2026 UCAR
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
 
-if( NOT CMAKE_BUILD_TYPE MATCHES "Debug" )
-  add_definitions( -DNDEBUG )
+
+# Set compiler flags for basic build types,
+# for compilers where this is not provided by ecbuild.
+include(build_type_compiler_flags)
+
+# Set JEDI's common compiler flags
+include(jedi_common_compiler_flags)
+
+# Set CRTM-specific compiler flags
+if(CMAKE_Fortran_COMPILER_ID STREQUAL GNU)
+  ecbuild_add_fortran_flags("-ffree-line-length-none")
 endif()
-
-#######################################################################################
-# Fortran
-#######################################################################################
-
-message(STATUS "Compiler ID: ${CMAKE_Fortran_COMPILER_ID}")
-message(STATUS "Compiler: ${CMAKE_Fortran_COMPILER}")
-
-set(CMAKE_FORTRAN_STANDARD 08)
-set(CMAKE_FORTRAN_STANDARD_REQUIRED ON)
-set(CMAKE_FORTRAN_EXTENSIONS OFF)
-
-if( CMAKE_Fortran_COMPILER_ID STREQUAL "GNU" )
-  include( compiler_flags_GNU_Fortran )
-
-elseif( CMAKE_Fortran_COMPILER_ID STREQUAL "IntelLLVM" )
-  include( compiler_flags_IntelLLVM_Fortran )
-
-elseif( CMAKE_Fortran_COMPILER_ID STREQUAL "Intel" )
-  include( compiler_flags_Intel_Fortran )
-
-elseif( CMAKE_Fortran_COMPILER_ID STREQUAL "PGI" OR CMAKE_Fortran_COMPILER_ID STREQUAL "NVHPC" )
-  include( compiler_flags_NVHPC_Fortran )
-
-elseif( CMAKE_Fortran_COMPILER_ID STREQUAL "XL" )
-  include( compiler_flags_XL_Fortran )
-
-elseif( CMAKE_Fortran_COMPILER_ID STREQUAL "Cray" )
-  include( compiler_flags_Cray_Fortran )
-
-else()
-  message( STATUS "Fortran compiler with ID ${CMAKE_Fortran_COMPILER_ID} will be used with CMake default options")
+if(CMAKE_Fortran_COMPILER_ID STREQUAL Intel)
+  ecbuild_add_fortran_flags("-assume byterecl")
+endif()
+if(CMAKE_Fortran_COMPILER_ID STREQUAL IntelLLVM)
+  # to match historical behavior, disable RelWithDebugInfo optimization for IntelLLVM
+  # TODO: determine source of problem and resolve in a more elegant way, then return
+  #       to default compiler flags so that RELWITHDEBINFO does what it says
+  set( CMAKE_Fortran_FLAGS_RELWITHDEBINFO "-O0 -g -DNDEBUG -traceback -heap-arrays 32" )
+  ecbuild_add_fortran_flags("-assume byterecl")
 endif()
