99a1fa84cd8cd5c4a3172869c910989f4c7114ef
Benjamin T. Johnson <Benjamin.T.Johnson@noaa.gov>
2026-06-05 15:31:11 -0400

    updating md5sum for tarball.

--- Get_CRTM_Binary_Files.sh
+++ Get_CRTM_Binary_Files.sh
@@ -4,7 +4,7 @@
 # The same files also download automatically during the cmake step, so you don't have to actually run this manually. 
 
 foldername="fix_REL-3.2.0.0"
-checksum=7386f5003071ab5075ac2644bfff786a #md5sum
+checksum=3dcef94c129efb78c85cdf542fca55ae #md5sum
 filename="${foldername}.tgz"
 download_url=https://bin.ssec.wisc.edu/pub/s4/CRTM/$filename
 
